313a55205257966183aa2069540b3bf033f044d6
JAVAD <MohammadJavadBabaeian88@gmail.com>
2025-08-24 11:03:44 +0330

    استایل فرم تغییر کرد

--- To Do List/src/components/TodoList.tsx
+++ To Do List/src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import style from "./style.module.css";
 interface task {
   id: number;
@@ -33,55 +33,84 @@ const TodoList = () => {
   const [describe, setDescribe] = useState<string>("");
   const [myTasks, setMyTasks] = useState<task[]>([]);
 
+  const taskForm = useRef<HTMLDivElement>(null);
+
   const handleAddTask = () => {
     const updatedTasks = addTask(myTasks, title, describe);
     setMyTasks(updatedTasks);
   };
+
+  const closeForm = () => {
+    if (taskForm.current) {
+      taskForm.current.style.display = 'none';
+    }
+  };
+
+  const openForm = () => {
+    if (taskForm.current) {
+      taskForm.current.style.display = 'block';
+    }
+  }
+
   return (
-    <>
-      <h3 style={{ margin: "0" }}>title</h3>
-      <input
-        style={{ display: "block", margin: "5px auto" }}
-        placeholder="Title..."
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }}
-        type="text"
-      />
-      <h3 style={{ margin: "0" }}>description</h3>
-      <input
-        style={{ display: "block", margin: "5px auto" }}
-        placeholder="Description..."
-        onChange={(e) => {
-          setDescribe(e.target.value);
-        }}
-        type="text"
-      />
-      <button style={{marginBottom:"10px"}} onClick={handleAddTask}>
-        Add Task
-      </button>
-      {myTasks.map((task) => (
-        <div
-          className={
-            task.isCompleted == false ? style.taskCard : style.taskCardCompleted
-          }
-          key={task.id}
-        >
-          <h4 style={{ margin: 0 }}>title: {task.title}</h4>
-          <h5 style={{ margin: 0 }}>describe: {task.description}</h5>
-          <h4 style={{ margin: 0, display: "inline" }}>
-            status: {task.isCompleted == true ? "Completed" : "Not Completed"}
-          </h4>
-          <input
-            onChange={() => {
-              setMyTasks(toggleTaskStatus(myTasks, task.id));
-            }}
-            type="checkbox"
-            checked={task.isCompleted}
-          />
+    <div className={style.continer}>
+      <div ref={taskForm} className={style.addTaskForm}>
+        <h6 onClick={closeForm} className={style.closeBtn}>
+          X
+        </h6>
+        <h3 className={style.titles}>Title</h3>
+        <input
+          className={style.inputs}
+          placeholder="Title..."
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          type="text"
+        />
+        <h3 className={style.titles}>Description</h3>
+        <input
+          className={style.inputs}
+          placeholder="Description..."
+          onChange={(e) => {
+            setDescribe(e.target.value);
+          }}
+          type="text"
+        />
+        <button className={style.button} onClick={handleAddTask}>
+          Add
+        </button>
+      </div>
+
+      <div>
+        <h3 style={{width:"500px"}}>To Do List</h3>
+        <div style={{background:"gray", width:"100%", borderRadius:"26px",marginBottom:"20px"}}>
+          <button onClick={openForm} className={style.openBtn}>+</button>
         </div>
-      ))}
-    </>
+        {myTasks.map((task) => (
+          <div
+            className={
+              task.isCompleted == false
+                ? style.taskCard
+                : style.taskCardCompleted
+            }
+            key={task.id}
+          >
+            <h4 style={{ margin: 0 }}>title: {task.title}</h4>
+            <h5 style={{ margin: 0 }}>describe: {task.description}</h5>
+            <h4 style={{ margin: 0, display: "inline" }}>
+              status: {task.isCompleted == true ? "Completed" : "Not Completed"}
+            </h4>
+            <input
+              onChange={() => {
+                setMyTasks(toggleTaskStatus(myTasks, task.id));
+              }}
+              type="checkbox"
+              checked={task.isCompleted}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
