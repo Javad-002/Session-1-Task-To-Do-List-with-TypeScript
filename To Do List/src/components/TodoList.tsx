51b34674a3af2e278e860d06ee8a13ce7322ff39
JAVAD <MohammadJavadBabaeian88@gmail.com>
2025-08-24 15:14:42 +0330

    استایل ها تغییر کرد + چالش تکمیل شد

--- To Do List/src/components/TodoList.tsx
+++ To Do List/src/components/TodoList.tsx
@@ -8,20 +8,35 @@ interface task {
   title: string;
   isCompleted: boolean;
   description?: string;
-  priority?: "high" | "medium" | "low";
+  priority: Priority;
 }
+
+type Priority = "high" | "medium" | "low" | "none";
+
+const priorityOrder: Record<task["priority"], number> = {
+  high: 1,
+  medium: 2,
+  low: 3,
+  none: 4,
+};
 
 const GenerateId = (tasks: task[]): number => {
   if (tasks.length === 0) return 1;
   return tasks.reduce((max, t) => Math.max(max, t.id), tasks[0].id) + 1;
 };
 
-const addTask = (tasks: task[], title: string, description: string): task[] => {
+const addTask = (
+  tasks: task[],
+  title: string,
+  description: string,
+  priority: Priority
+): task[] => {
   const newTask: task = {
     id: GenerateId(tasks),
     title,
     isCompleted: false,
     description,
+    priority,
   };
   return [...tasks, newTask];
 };
@@ -33,7 +48,7 @@ function toggleTaskStatus(tasks: task[], id: number): task[] {
 }
 
 const TaskSchema = Yup.object().shape({
-  title: Yup.string().required("عنوان الزامی است"),
+  title: Yup.string().required("Title is required"),
 });
 
 const TodoList = () => {
@@ -60,13 +75,14 @@ const TodoList = () => {
         </h6>
 
         <Formik
-          initialValues={{ title: "", description: "" }}
+          initialValues={{ title: "", description: "", priority: "none" }}
           validationSchema={TaskSchema}
           onSubmit={(values, { resetForm }) => {
             const updatedTasks = addTask(
               myTasks,
               values.title,
-              values.description
+              values.description,
+              values.priority
             );
             setMyTasks(updatedTasks);
             resetForm();
@@ -93,11 +109,14 @@ const TodoList = () => {
                 placeholder="Description..."
                 className={style.inputs}
               />
-              <ErrorMessage
-                name="description"
-                component="div"
-                className="text-red-500 text-sm"
-              />
+
+              <h3 className={style.titles}>priority</h3>
+              <Field as="select" name="priority" className={style.inputs}>
+                <option value="none">Select priority</option>
+                <option value="high">High</option>
+                <option value="medium">Medium</option>
+                <option value="low">Low</option>
+              </Field>
 
               <button
                 type="submit"
@@ -125,29 +144,39 @@ const TodoList = () => {
             +
           </button>
         </div>
-        {myTasks.map((task) => (
-          <div
-            className={
-              task.isCompleted === false
-                ? style.taskCard
-                : style.taskCardCompleted
-            }
-            key={task.id}
-          >
-            <h4 style={{ margin: 0 }}>title: {task.title}</h4>
-            <h5 style={{ margin: 0 }}>describe: {task.description}</h5>
-            <h4 style={{ margin: 0, display: "inline" }}>
-              status: {task.isCompleted ? "Completed" : "Not Completed"}
-            </h4>
-            <input
-              onChange={() => {
-                setMyTasks(toggleTaskStatus(myTasks, task.id));
-              }}
-              type="checkbox"
-              checked={task.isCompleted}
-            />
-          </div>
-        ))}
+
+        {[...myTasks]
+          .sort((a, b) => priorityOrder[a.priority] - priorityOrder[b.priority])
+          .sort((a, b) => Number(a.isCompleted) - Number(b.isCompleted))
+          .map((task) => (
+            <div
+              className={
+                task.isCompleted === false
+                  ? style.taskCard
+                  : style.taskCardCompleted
+              }
+              key={task.id}
+            >
+              <h4 className="m-0 text-xl font-bold">{task.title}</h4>
+              {task.description != "" ? (
+                <h5 className="m-0 bg-neutral-700 p-1">{task.description}</h5>
+              ) : null}
+              {task.priority != "none" ? (
+                <h4 className="m-0">priority: {task.priority}</h4>
+              ) : null}
+              <h4 className="m-0">
+                status: {task.isCompleted ? "Completed" : "Not Completed"}
+              </h4>
+              <input
+                onChange={() => {
+                  setMyTasks(toggleTaskStatus(myTasks, task.id));
+                }}
+                type="checkbox"
+                checked={task.isCompleted}
+                className="absolute top-4 right-4 size-5"
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
